c571c68574a15c1c4765a2cdb1e629a718a7a520
Tony Rowan <trowan812@gmail.com>
2020-12-06 17:13:27 +0000

    Day 06, part 1: correct

---
+++ day_06/part_1.rb
@@ -0,0 +1,33 @@
+require 'awesome_print'
+require 'set'
+require_relative '../helpers/input'
+
+class Group
+  attr_reader :answers
+
+  def self.from_input
+    groups = []
+    current_group_input = []
+
+    process_input do |line|
+      if line == ''
+        groups << Group.new(current_group_input)
+        current_group_input = []
+        next
+      end
+
+      current_group_input << line
+    end
+
+    groups << Group.new(current_group_input)
+
+    groups
+  end
+
+  def initialize(group_input)
+    @answers = Set.new(group_input.reduce(:+).chars)
+  end
+end
+
+
+p Group.from_input.map(&:answers).map(&:size).reduce(:+)
