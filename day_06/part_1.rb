3a81adcc915f92c4f4cc419e5adc0775bec8120a
Tony Rowan <trowan812@gmail.com>
2020-12-06 17:14:51 +0000

    Move Group to own file

--- day_06/part_1.rb
+++ day_06/part_1.rb
@@ -1,33 +1,3 @@
-require 'awesome_print'
-require 'set'
-require_relative '../helpers/input'
-
-class Group
-  attr_reader :answers
-
-  def self.from_input
-    groups = []
-    current_group_input = []
-
-    process_input do |line|
-      if line == ''
-        groups << Group.new(current_group_input)
-        current_group_input = []
-        next
-      end
-
-      current_group_input << line
-    end
-
-    groups << Group.new(current_group_input)
-
-    groups
-  end
-
-  def initialize(group_input)
-    @answers = Set.new(group_input.reduce(:+).chars)
-  end
-end
-
+require_relative './group'
 
 p Group.from_input.map(&:answers).map(&:size).reduce(:+)
