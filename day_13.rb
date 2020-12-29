a45244c1a5b22c3f2bd375b0eb2053884f194b58
Tony Rowan <trowan812@gmail.com>
2020-12-29 01:16:11 +0000

    Day 13, Part 2: Correct
    With help from: https://www.reddit.com/r/adventofcode/comments/klmrry/2020_day_13_part_2_my_brain_cant_comprehend_why/

--- day_13.rb
+++ day_13.rb
@@ -14,13 +14,37 @@ module Day13
     end
   end
 
+  def self.solve_part_2
+    departure_schedule = notes[:departure_schedule]
+    timestep = 0
+    delta = 1
+
+    loop do
+      valid = true
+
+      departure_schedule.each do |time, bus_id|
+        if (timestep + time) % bus_id != 0
+          valid = false
+          break
+        end
+
+        delta = delta.lcm(bus_id)
+      end
+
+      return timestep if valid
+
+      timestep += delta
+    end
+  end
+
   def self.notes
     lines = []
     process_input(13) { |line| lines << line }
     estimated_departure_time, bus_ids = lines
     {
       estimated_departure_time: estimated_departure_time.to_i,
-      bus_ids: bus_ids.split(',').map(&:to_i).compact.reject(&:zero?)
+      bus_ids: bus_ids.split(',').map(&:to_i).compact.reject(&:zero?),
+      departure_schedule: bus_ids.split(',').each_with_index.map { |elem, idx| [elem, idx] }.reject { |elem, idx| elem == 'x' }.inject({}) { |hash, elem_idx| hash[elem_idx[1]] = elem_idx[0].to_i; hash }
     }
   end
 end
